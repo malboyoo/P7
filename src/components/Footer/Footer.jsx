05eebf0b58a9739cd6d1316dd5496c4c1f827797
Malboyoo <malboyoo@gmail.com>
2023-02-10 09:48:55 +0100

    final version

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import styles from "./Footer.module.scss";
 import logoWhite from "../../assets/images/Logo_Kasa_white.png";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className={`${styles.footer} flex-col align-items-center justify-content-center`}>
-      <img src={logoWhite} alt="Logo de l'agence Kasa" />
+      <Link to="/">
+        <img src={logoWhite} alt="Logo de l'agence Kasa" />
+      </Link>
       <p className="mb-30">© 2020 Kasa. All rights reserved</p>
     </footer>
   );
